f35605868bde0ef55324172445ba58f6eeccd709
dumpy <andy@dumpyy.xyz>
2022-05-19 16:24:00 +1000

    update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="realme community docs!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
